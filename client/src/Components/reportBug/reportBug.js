a5ca4aeee2f510eaa22dd9176bd7c5c3e7ae208a
aparekh5 <aryamanparekh12@gmail.com>
2020-06-13 12:59:11 +0530

    Modified the session schema

---
+++ client/src/Components/reportBug/reportBug.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class reportBug extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
---
+++ client/src/MongoDB Models/SessionSchema.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema;
+
+const Session = new Schema({
+    _id : {type: String},
+    videoLink: { type: Array,},
+    hostName: { type: String, required:true},
+    userEditAccess: { type: Boolean, default:false},
+    vidNum : {type: Number, default: 0},
+    users : {type: Array},
+    lastPlayBack : {type: Number, default: 0}
+});
+
+const SessionModel = mongoose.model('Sessions', Session);
+
+module.exports= {SessionModel};   
