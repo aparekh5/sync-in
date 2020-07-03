7c21128cf5badf340a9c54e7fdefe74f767477f3
aparekh5 <aryamanparekh12@gmail.com>
2020-07-03 10:56:26 +0530

    Modified the nav bar to become more responsive

--- client/src/Components/Navbar/Navbar.js
+++ client/src/Components/Navbar/Navbar.js
@@ -6,15 +6,17 @@ export default class Navbar extends Component {
 
     render() {
         return (
-            <div className='navbar'>
-                <Link to='/' className='Logo'> Sync In</Link>
-                <ul className='nav-links'>
-                    <li><Link to='/'>Host</Link></li>
-                    <li><a className='bug' href='https://github.com/aparekh5/sync-in-bug-reporting' target='_blank'>Report A Bug</a></li>
-                    
-                    <li><a href='https://www.paypal.me/parekharyaman' target='_blank'>Support Us</a></li>
-                </ul>
+            <div>
+                <div className='navbar'>
+                    <Link to='/' className='Logo'> Sync In</Link>
+                    <ul className='nav-links'>
+                        <li><Link to='/host'>Host</Link></li>
+                        <li><a className='bug' href='https://github.com/aparekh5/sync-in-bug-reporting' target='_blank'>Report A Bug</a></li>
+                       
+                    </ul>
+                </div>
+                
             </div>
-        )
+        );
     }
 }
